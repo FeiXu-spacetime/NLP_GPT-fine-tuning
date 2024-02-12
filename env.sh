a695dd7df78b479d8a50ad9efb8fa0111497f788
Fay Xu <fx2024@slurm.ttic.edu>
2024-02-12 03:13:44 -0600

    multi GPU

---
+++ env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+eval "$(/home-nfs/fx2024/mc3/bin/conda 'shell.bash' 'hook')"
+conda activate fei-venv
--- my_dataset.py
+++ my_dataset.py
@@ -8,11 +8,9 @@ class My_Dataset(Dataset):
 
         self.X = textdata['text'][:64]
         pdb.set_trace()
-        print(self.X[:10])
-        
-
+        #print(self.X[:10])
         #self.X_encoded = tokenizer(self.X, truncation=True, padding=True, return_tensors="pt")
-        self.X_encoded = tokenizer(self.X,max_length=20, truncation=True, padding="max_length", return_tensors="pt")
+        self.X_encoded = tokenizer(self.X, max_length=20, truncation=True, padding="max_length", return_tensors="pt")
         self.input_ids = self.X_encoded['input_ids'].cuda() # number of rows X max_length
         self.attention_mask = self.X_encoded['attention_mask'].cuda()
         pdb.set_trace()
